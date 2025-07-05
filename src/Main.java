f796706031cec320a8615831e8f42a0dc1a10155
Lucas Viganó da Silva <lucasviganodasilva@gmail.com>
2025-07-05 14:08:53 -0300

    Criação do tabuleiro do jogo

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    public int boardSize = 9;
+
     public static void main(String[] args) {
-        System.out.println("Rodando");
+      int [][] sudokuGame = {
+            {5, 0, 4, 0, 6, 3, 0, 0, 1},
+            {0, 0, 0, 9, 0, 0, 0, 0, 7},
+            {1, 0, 0, 7, 0, 0, 0, 3, 0},
+            {0, 8, 6, 1, 0, 7, 0, 5, 0},
+            {3, 5, 0, 0, 2, 0, 0, 7, 6},
+            {0, 1, 0, 3, 0, 6, 9, 4, 0},
+            {0, 4, 0, 0, 0, 2, 0, 0, 5},
+            {8, 0, 0, 0, 0, 9, 0, 0, 0},
+            {7, 0, 0, 6, 3, 0, 4, 0, 9},
+      };
     }
 }
